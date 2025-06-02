22daa17184496dc41201bd906be536bc82ff8bd5
Sheepy <mrcake258@gmail.com>
2025-06-02 17:58:55 +0300

    v terminale statistika

--- worker_training.py
+++ worker_training.py
@@ -1,24 +1,20 @@
 from PyQt5.QtCore import QObject, pyqtSignal
-from PyQt5.QtWidgets import QProgressBar, QPlainTextEdit
 from defect_classification import run_training
 
 class TrainingWorker(QObject):
     finished = pyqtSignal(object, str, list, list, list, tuple)
     update_text = pyqtSignal(str)
-    update_status = pyqtSignal(str)
 
-    def __init__(self, progress_bar, output_box, text_signal, status_signal):
+    def __init__(self, progress_bar, output_box, text_signal):
         super().__init__()
         self.progress_bar = progress_bar
         self.output_box = output_box
         self.text_signal = text_signal
-        self.status_signal = status_signal
 
     def run(self):
         history, base_path, data_set, sample_images, augmented_images, cm_data = run_training(
             progress_bar=self.progress_bar,
             text_output=self.output_box,
-            text_signal=self.text_signal,
-            status_signal=self.status_signal
+            text_signal=self.text_signal
         )
         self.finished.emit(history, base_path, data_set, sample_images, augmented_images, cm_data)
